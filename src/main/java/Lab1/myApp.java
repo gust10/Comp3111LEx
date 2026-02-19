6a903d254c9c58838fd20638fffe691d44f03725
gust10 <2005hyunsung@gmail.com>
2026-02-19 15:58:50 +0800

    Create java source codes

---
+++ src/main/java/Lab1/myApp.java
@@ -0,0 +1,14 @@
+package Lab1;
+import Lab1.myLibrary;
+
+public class myApp {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int exp = 4;
+
+        System.out.println("Factorial of "+num1+": "+ myLibrary.factorial(num1));
+        System.out.println(" "+num1 +" power of "+exp + " will be " + myLibrary.Power(num1, exp));
+        System.out.println("Done!!");
+    }
+}
